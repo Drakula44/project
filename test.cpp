871c550cb63731bd668d2192072bd406bc316322
Drakula44 <ninadrakulic04@gmail.com>
2021-05-22 16:13:06 +0200

    dodata funkcija za ispisivanje kvadarata

--- test.cpp
+++ test.cpp
@@ -13,6 +13,16 @@ void triangle(int n)
     }
 }
 
+void square(int n)
+{
+    for(int i = 1; i <= n;i++)
+    {
+        for(int j = 1;j <= n;j++)
+            cout << "*";
+        cout << endl;
+    }
+}
+
 int main()
 {
     triangle(4);
