f15df41fb0e4621e26ed3757f10c576c3a9f3482
Drakula44 <ninadrakulic04@gmail.com>
2021-05-22 15:34:51 +0200

    prvi komit

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include "test.h"
+
+using namespace std;
+
+void triangle(int n)
+{
+    for(int i = 1; i <= n;i++)
+    {
+        for(int j = 1;j <= i;j++)
+            cout << "*";
+        cout << endl;
+    }
+}
+
+int main()
+{
+    triangle(4);
+}
