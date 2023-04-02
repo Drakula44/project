d8e980a6059c6409ee05999b2942d11caa72b346
OP5642 <ognjenpetrov@yahoo.com>
2023-04-02 20:05:59 +0200

    Added function to .h

--- test.cpp
+++ test.cpp
@@ -25,8 +25,22 @@ void square(int n)
     }
 }
 
+void empty_square(int n)
+{
+    for(int i = 1; i <= n;i++)
+    {
+        for(int j = 1;j <= n;j++)
+			if(i==1 || j==1 || i==n || j==n)
+            	cout << "*";
+			else
+				cout << " ";
+        cout << endl;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     int n = atoi(argv[1]);
     triangle(n);
+	empty_square(n);
 }
